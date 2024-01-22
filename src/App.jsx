18d11369f176809efa87804f6d4bad1fad0ea2ff
hcokim <62118407+hcokim@users.noreply.github.com>
2024-01-21 21:30:31 -0500

    first pass at react

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,37 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import nosleep from 'nosleep.js'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App() {
+
+	const noSleep = new nosleep();
+	const [count, setCount] = useState(0);
+	const [wakeLock, setWakeLock] = useState(false);
+
+	const btnLabel = wakeLock ?
+		'Stop bowing' : 'Start bowing';
+
+	function toggleWake() {
+		setWakeLock(prev => {
+			if (prev) {
+				noSleep.disable();
+			} else {
+				noSleep.enable();
+			}
+			return !prev;
+		});
+	}
+
+	return (
+		<div className="App">
+			<h1>Bow Counter</h1>
+			<h1>{count}</h1>
+			<button onMouseDown={toggleWake}>{btnLabel}</button>
+			<button onMouseDown={() => setCount(prev => prev + 1)}>Bow</button>
+			{!wakeLock && <button>Reset</button>}
+		</div>
+	)
 }
 
 export default App
