9e83f0a21529ef6a11bcee9db4544dc47c5aa17e
hcokim <62118407+hcokim@users.noreply.github.com>
2024-01-22 15:50:19 -0500

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      includeAssets: ['favicon.svg', 'mask-icon.svg', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Bow Counter',
+        short_name: 'BowCounter',
+        theme_color: '#474747',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
